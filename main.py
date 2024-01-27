acc16e99376ea60f7f2c31117f82a7fbfbd130db
jacquescogal <jacques.tracy@gmail.com>
2024-01-27 16:55:43 +0800

    feat: remove swagger in production

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from src.routers.auth_routers import auth_router
+import os
 load_dotenv()
-
-app = FastAPI()
+environment = os.getenv("ENVIRONMENT")
+app = FastAPI(docs_url=None if environment== "production" else "/docs")
 
 app.include_router(auth_router)
 
