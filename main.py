12a0fb66584f19498e2b03614978bb09361323ad
jacquescogal <jacques.tracy@gmail.com>
2024-01-27 19:52:46 +0800

    feat: add middleware

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from src.routers.auth_routers import auth_router
 import os
+
 load_dotenv()
 environment = os.getenv("ENVIRONMENT")
 app = FastAPI(docs_url=None if environment== "production" else "/docs")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True, # Indicates whether to support credentials
+    allow_methods=["*"],    # Allows all methods
+    allow_headers=["*"],    # Allows all headers
+)
+
 
 app.include_router(auth_router)
 
