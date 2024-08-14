55aa9edb2a15d3dfb071469ab31932b8b9be2204
jacques002 <jacques002@e.ntu.edu.sg>
2024-08-15 00:15:59 +0800

    fix: cors origin

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI(docs_url=None if environment== "production" else "/docs")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["https://jacquescogal.github.io"],  # Allows all origins
     allow_credentials=True, # Indicates whether to support credentials
     allow_methods=["*"],    # Allows all methods
     allow_headers=["*"],    # Allows all headers
